43703f6073ccb8aac060d36f0a91293eb3b465c6
Cijun Zhu <cijun.zhu@gmail.com>
2020-02-21 13:31:26 -0500

    mapbox gl basic

--- src/pages/map/map-screen.tsx
+++ src/pages/map/map-screen.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import MapboxGL from "@react-native-mapbox-gl/maps";
+import { View, StyleSheet } from 'react-native';
 
 export const MapScreen = () => {
-    return <></>
+
+    MapboxGL.setAccessToken("pk.eyJ1IjoibmdrZiIsImEiOiJjajAydTdyc3MwNnI0MndsajJoMGlpN3ZyIn0.BGIZja4zlym3f4AS3olLhg");
+
+    const styles = StyleSheet.create({
+        page: {
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#F5FCFF"
+        },
+        container: {
+          height: 300,
+          width: 300,
+          backgroundColor: "tomato"
+        },
+        map: {
+          flex: 1
+        }
+      });
+
+      MapboxGL.setTelemetryEnabled(false);
+     
+
+    return <>
+        <View style={styles.page}>
+        <View style={styles.container}>
+          <MapboxGL.MapView style={styles.map} />
+        </View>
+      </View>
+    </>
 };
